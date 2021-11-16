acd67835e3d18df9e34e8c87c76aac402f76ffa1
Francesca Pompi <francesca.pompi93@gmail.com>
2021-11-16 15:49:28 +0100

    fine programma

--- src/org/generation/italy/CalcolaBiglietto.java
+++ src/org/generation/italy/CalcolaBiglietto.java
@@ -1,10 +1,35 @@
 package org.generation.italy;
 
+import java.util.Scanner;
+
 public class CalcolaBiglietto {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		// input variables
+		double numberOfKilometers;
+		int age;
+		
+		System.out.print("Insert number of kilometers: ");
+		numberOfKilometers = scanner.nextDouble();
+		System.out.print("Insert age: ");
+		age = scanner.nextInt();
+		
+		double ticketPrice = 0.21 * numberOfKilometers;
+		double minorsDiscount = ticketPrice - (ticketPrice / 100 * 20);
+		double seniorDiscount = ticketPrice - (ticketPrice / 100 * 40);
+		
+		if(age < 18) {
+			System.out.print("The ticket costs: " + minorsDiscount);
+		} else if(age > 65) {
+			System.out.print("The ticket costs: " + seniorDiscount);
+		} else {
+			System.out.print("The ticket costs: " + ticketPrice);
+		}
 		
 
+		scanner.close();
 	}
 
 }
